8f723db0a233a36c32595de31085d972967cb3ca
Awortwe Daniel <123452581+AwortweDaniel@users.noreply.github.com>
2025-03-15 13:28:00 +0000

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,7 @@
 import yagmail
 
 # Replace with your actual email and password
-email = "nanadanieljr@gmail.com"
-password = "rctsncaiajjkgfbl"
+
 
 # Register the email credentials securely in the keyring
 yagmail.register(email, password)
